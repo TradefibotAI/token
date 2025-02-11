6248493d6cce2c18bd7f97ee3ad30bc7078e47f2
Alfredo Ramirez <alframirezg@gmail.com>
2025-02-11 18:21:59 -0400

    updating token

--- TDFI.sol
+++ TDFI.sol
@@ -5,75 +5,76 @@
 // Twitter: https://x.com/Tradefibot
 // Telegram: https://t.me/TradefibotChat
 // ⁠Instagram: https://www.instagram.com/tradefibot
-// ⁠Youtube Channel: https://www.youtube.com/@Tradefibot 
+// ⁠Youtube Channel: https://www.youtube.com/@Tradefibot
 
 pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 
-contract TDFI is
-    ERC20,
-    ERC20Burnable,
-    ERC20Permit
-{
-    uint256 public immutable maxSupply;
+contract TDFI is ERC20, ERC20Burnable, ERC20Permit {
+    uint256 public immutable maxSupply = 150000000000000000000000000;
 
-   // Project Wallets 
-    address public constant reserveWallet = 
-        address(0x245bf5E201F54Ef490897C1524C478791430DF0F); 
-    uint256 public constant reservePercentage = 40; 
- 
-    address public constant airDropWallet = 
-        address(0x0d6c9c8B20E486742b31e1D0592E33686C7F038C); 
-    uint256 public constant airDropPercentage = 40; 
- 
-    address public constant publicPresaleWallet = 
-        address(0xF193C174165d1fd7adc4D2DD70ceF5747D75Aec5); 
-    uint256 public constant publicPresalePercentage = 380; 
- 
-    address public constant privatePresaleWallet = 
-        address(0x1820ff6D2EEb9D9Af2C2148ceB8096fcB9FC8ce6); 
-    uint256 public constant privatePresalePercentage = 20; 
- 
-    address public constant liquidityWarrantyWallet = 
-        address(0xF8726B9943bAEb52Db32ec2C80D90c0e9B519044); 
-    uint256 public constant liquidityWarrantyPercentage = 400; 
- 
-    address public constant developmentWallet = 
-        address(0xeD75eBa6932caF1F84FAe3a11651B10bAb315Bc3); 
-    uint256 public constant developmentPercentage = 60; 
- 
-    address public constant marketingWallet = 
-        address(0x522140F9970e5497e5568cc09C69ab3FF9F2F6eF); 
-    uint256 public constant marketingPercentage = 60;
+    // Project Wallets
+    address public immutable seedPresaleWalletA = address(0xB3219929716d152025CBF44ae6251eFf1522c87b);
+    uint256 public immutable seedPresalePercentage = 300;
+
+    address public immutable privatePresaleWalletB =
+        address(0x680CE202cb9a48791d069cA1D374c21D53Cd997E);
+    uint256 public immutable privatePresalePercentageB = 170;
+
+        address public immutable privatePresaleWalletC =
+        address(0xce8F9545548156e29e259e3a51FF3C20413466F4);
+    uint256 public immutable privatePresalePercentageC = 1350;
+
+    address public immutable publicPresaleWallet =
+        address(0x647cF37D0176E4De0422aC1e74802e645e00FD04);
+    uint256 public immutable publicPresalePercentage = 38180;
+
+    address public immutable liquidityWallet =
+        address(0xAeBFDC682d88F1Aa79Deee769d58921381333D89);
+    uint256 public immutable liquidityPercentage = 40000;
+
+    address public immutable developmentWallet =
+        address(0xe06ded0573864ef4Ee647Fa9A96b42397Fb871ee);
+    uint256 public immutable developmentPercentage = 6000;
+
+    address public immutable marketingWallet =
+        address(0xE6B72b45217522538890FC1Ae9A0888bf40281D6);
+    uint256 public immutable marketingPercentage = 6000;
+
+    address public immutable treasuryWallet =
+        address(0x92b4eFAf6A42E35a5A7Dbaf64fA58055803CCE1c);
+    uint256 public immutable treasuryPercentage = 4000;
+
+    address public immutable growthWallet =
+        address(0x80f574A044F5D77ba94c29C04941288B182f88DF);
+    uint256 public immutable growthPercentage = 4000;
+
+    address public immutable dividendsWallet =
+        address(0x441E1E6AD328d43C66e5D3d79D1959D2bc28185f);
+
+    address public immutable burnWallet =
+        address(0x000000000000000000000000000000000000dEaD);
+
+    address public daoWallet =
+        address(0x01BEED82101Dacf3a8D1f57d000878B20CdE901B);
 
     IUniswapV2Router02 private uniswapV2Router;
     address public immutable uniswapV2Pair;
 
-    address public immutable projectWallet;
-    address public immutable jackpotWallet;
-    address public immutable burnWallet;
-
-    uint16 public immutable sellTax;
+    uint16 public immutable sellTax = 5;
 
     mapping(address => bool) public automatedMarketMakerPairs;
 
-    address public admin;
 
-    constructor(
-        uint256 setMaxSupply
-    ) ERC20("Tradefi.bot", "TDFI") ERC20Permit("Tradefi.bot") {
-
-        admin = msg.sender;
-
-        // Defining the MaxSupply
-        maxSupply = setMaxSupply;
-
+    constructor()
+        ERC20("Tradefi.bot Token", "TDFI")
+        ERC20Permit("Tradefi.bot Token")
+    {
         // Uniswap Router
         IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(
             0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24
@@ -85,55 +86,56 @@ contract TDFI is
         uniswapV2Router = _uniswapV2Router;
         uniswapV2Pair = _uniswapV2Pair;
 
-        sellTax = 5;
-        projectWallet = publicPresaleWallet;
-        jackpotWallet = reserveWallet;
-        burnWallet = liquidityWarrantyWallet;
-
         _setAutomatedMarketMakerPair(_uniswapV2Pair, true);
 
         // Mint the initial supply
-        _initalSupply();
+        _initialSupply();
     }
 
-    modifier onlyAdmin() {
-        require(msg.sender == admin, "Only the admin can perform this action");
+    modifier onlyDAO() {
+        require(
+            msg.sender == daoWallet,
+            "Only the DAO can perform this action"
+        );
         _;
     }
 
-    function updateAdmin(address _newAdmin) public onlyAdmin {
-        admin = _newAdmin;
+    function updateDao(address _newDao) public onlyDAO {
+        daoWallet = _newDao;
     }
 
     // Initial Supply
-    function _initalSupply() private {
-        _mint(reserveWallet, (maxSupply * reservePercentage) / 1000);
-        _mint(airDropWallet, (maxSupply * airDropPercentage) / 1000);
+    function _initialSupply() private {
+        _mint(seedPresaleWalletA, (maxSupply * seedPresalePercentage) / 1000);
+        _mint(
+            privatePresaleWalletB,
+            (maxSupply * privatePresalePercentageB) / 1000
+        );
+        _mint(
+            privatePresaleWalletC,
+            (maxSupply * privatePresalePercentageC) / 1000
+        );
+        
         _mint(
             publicPresaleWallet,
             (maxSupply * publicPresalePercentage) / 1000
         );
-        _mint(
-            privatePresaleWallet,
-            (maxSupply * privatePresalePercentage) / 1000
-        );
-        _mint(
-            liquidityWarrantyWallet,
-            (maxSupply * liquidityWarrantyPercentage) / 1000
-        );
+        _mint(liquidityWallet, (maxSupply * liquidityPercentage) / 1000);
         _mint(marketingWallet, (maxSupply * marketingPercentage) / 1000);
         _mint(developmentWallet, (maxSupply * developmentPercentage) / 1000);
+        _mint(treasuryWallet, (maxSupply * treasuryPercentage) / 1000);
+        _mint(growthWallet, (maxSupply * growthPercentage) / 1000);
+        
     }
 
     // events
     event SetAutomatedMarketMakerPair(address indexed pair, bool indexed value);
 
-    // Uniswap logic functions
-
-    function setAutomatedMarketMakerPair(
-        address pair,
-        bool value
-    ) public onlyAdmin {
+    // Manage AMM pairs by DAO
+    function setAutomatedMarketMakerPair(address pair, bool value)
+        public
+        onlyDAO
+    {
         require(
             pair != uniswapV2Pair,
             "The Uniswap pair cannot be removed from automatedMarketMakerPairs"
@@ -152,35 +154,33 @@ contract TDFI is
         emit SetAutomatedMarketMakerPair(pair, value);
     }
 
-    // Logic for taxes
+    // Override transfer to apply sell tax for DEX trades
     function _transfer(
         address from,
         address to,
         uint256 amount
     ) internal override {
-
-        // Sell Taxes
         if (automatedMarketMakerPairs[to]) {
-                    // Sell
-                    uint256 _tax = (amount * sellTax) / 100;
-                    uint256 _projectTax = (_tax * 25) / 100;
-                    uint256 _burnTax = (_tax * 25) / 100;
-                    uint256 _jackpotTax = _tax - _projectTax - _burnTax;
-                    amount = amount - _tax;
+            // Sell
+            uint256 fee = (amount * sellTax) / 100;
+            uint256 treasuryAmount = (fee * 25) / 100;
+            uint256 burnAmount = (fee * 25) / 100;
+            uint256 dividendsAmount = fee - treasuryAmount - burnAmount;
 
-                    super._transfer(from, projectWallet, _projectTax);
-                    emit Transfer(from, projectWallet, _projectTax);
+            // Distribute tax
+            super._transfer(from, treasuryWallet, treasuryAmount);
+            emit Transfer(from, treasuryWallet, treasuryAmount);
 
-                     super._transfer(from, burnWallet, _burnTax);
-                    emit Transfer(from, burnWallet, _burnTax);
+            super._transfer(from, burnWallet, burnAmount);
+            emit Transfer(from, burnWallet, burnAmount);
 
-                     super._transfer(from, jackpotWallet, _jackpotTax);
-                    emit Transfer(from, jackpotWallet, _jackpotTax);
+            super._transfer(from, dividendsWallet, dividendsAmount);
+            emit Transfer(from, dividendsWallet, dividendsAmount);
 
-
-                    super._transfer(from, to, amount);
-                    emit Transfer(from, to, amount);
+            super._transfer(from, to, amount);
+            emit Transfer(from, to, amount);
         } else {
+            // Standard transfer for non-DEX trades
             super._transfer(from, to, amount);
             emit Transfer(from, to, amount);
         }
